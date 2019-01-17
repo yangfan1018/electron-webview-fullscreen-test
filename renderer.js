6fb7cb68b93c1796762ae0228056480ac5ea540b
yangyangfan <Varianyang@51jiecai.com>
2019-01-17 16:06:29 +0800

    up

--- renderer.js
+++ renderer.js
@@ -1,3 +1,13 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+let webview = document.querySelectorAll('webview')[0]
+
+webview.addEventListener('dom-ready', () => {
+    webview.executeJavaScript(`
+        alert(document.webkitFullscreenEnabled)
+    `)
+    webview.addEventListener('enter-html-full-screen', (e)=>{
+        console.log(e)
+    })
+})
